d853567c75b5d8974b886822cc7947b04d09e2ff
Norbert Sagadyn <norbert.sag@gmail.com>
2024-10-16 19:13:58 +0200

    Deleted comments

--- src/app/services/invoice-service.service.ts
+++ src/app/services/invoice-service.service.ts
@@ -5,17 +5,14 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class InvoiceService {
-  // BehaviorSubject do przechowywania pozycji towarów
   private itemsSource = new BehaviorSubject<any[]>([]);
 
   constructor() {}
 
-  // Funkcja do aktualizacji listy pozycji towarów
   updateItems(items: any[]) {
     this.itemsSource.next(items);
   }
 
-  // Funkcja do pobrania bieżącej listy towarów
   getItems() {
     return this.itemsSource.getValue();
   }
